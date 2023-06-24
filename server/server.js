317d89bad7ca86d90954aa20909afc0d9d9b114e
Paulo Lopez <geekcrasher@gmail.com>
2023-06-24 23:33:57 +0800

    imported require package for the server

---
+++ server/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const port = process.env.PORT || 4000
+
+const app = express()
+
+app.listen(port, () => {
+   console.log(`Server running on port ${port}`)
+})
