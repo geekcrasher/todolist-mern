65767ade16690092af00ff73d25f143a5883bec4
Paulo Lopez <geekcrasher@gmail.com>
2023-06-28 00:52:11 +0800

    imported the all  routes from todoRouter

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-require('dotenv').config()
-
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
 const port = process.env.PORT || 4000
 
 const app = express()
+
+const todoRouter = require('./routes/todoRouter')
+
+
+app.get('/api/todo', todoRouter)
 
 app.listen(port, () => {
    console.log(`Server running on port ${port}`)
