95e96ba7c3d06a2230618a6a795f8c1329f75f65
Paulo Lopez <geekcrasher@gmail.com>
2023-06-28 23:45:47 +0800

    updating the server and root folder

--- server/routes/todoRouter.js
+++ server/routes/todoRouter.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
-
+// get all tasks
 router.get('/', (req, res) => {
-   res.status(200).json({ msg: 'Get Todos' })
+   res.status(200).json({ msg: 'Get Tasks' })
 })
 
+// add a new task
 router.post('/', (req, res) => {
    res.status(200).json({ msg: 'Add Todo' })
 })
 
-
+// update a task
 router.put('/:id', (req, res) => {
    res.status(200).json({ msg: 'Update Todo' })
 })
 
+// delete a task
 router.delete('/:id', (req, res) => {
    res.status(200).json({ msg: 'Delete Todo' })
 })
