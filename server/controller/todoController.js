c5c95ed9d237b4e89c32d05cda81f81a8eff27e3
Paulo Lopez <geekcrasher@gmail.com>
2023-06-29 23:17:18 +0800

    added async in the controller

--- server/controller/todoController.js
+++ server/controller/todoController.js
@@ -1,25 +1,25 @@
 
 // get todo
 // route -> GET /api/todos
-const getTodo = (req, res) => {
+const getTodo = async (req, res) => {
    res.status(200).json({ msg: 'Get Tasks' })
 }
 
 // post todo
 // route -> POST /api/todos
-const addTodo = (req, res) => {
+const addTodo = async (req, res) => {
    res.status(200).json({ msg: 'Add Todo' })
 }
 
 // update todo
 // route -> PUT /api/todos/:id
-const updateTodo = (req, res) => {
+const updateTodo = async (req, res) => {
    res.status(200).json({ msg: 'Update Todo' })
 }
 
 // delete todo
 // route -> DELETE /api/todos/:id
-const deleteTodo = (req, res) => {
+const deleteTodo = async (req, res) => {
    res.status(200).json({ msg: 'Delete Todo' })
 }
 
