2eff9c2b5beb0a5afc5257e5f5a02b1d38f5561e
Paulo Lopez <geekcrasher@gmail.com>
2023-07-01 00:14:34 +0800

    imported database connection

--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
+const dotenv = require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 4000
+const connectDB = require('./config/connectDB')
+const PORT = process.env.PORT || 4000
 
 const app = express()
 
 const todoRouter = require('./routes/todoRouter')
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/todos', todoRouter)
 
-app.listen(port, () => {
-   console.log(`Server running on port ${port}`)
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`)
 })
