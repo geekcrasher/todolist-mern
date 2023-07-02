7b370375ce59131bad3a5bd151f04b06ef310a0b
Paulo Lopez <geekcrasher@gmail.com>
2023-07-02 23:57:31 +0800

    fix the name of the reducers

--- client/src/components/CreateTask.jsx
+++ client/src/components/CreateTask.jsx
@@ -1,9 +1,85 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setTodo,
+  setTitle,
+  setDescription,
+  setPriority
+} from "../features/taskReducer";
+
 const CreateTask = () => {
-   return ( 
-      <section>
-         
-      </section>
-    );
-}
- 
+  const dispatch = useDispatch();
+
+  const {todo, title, description, priority } = useSelector(
+    (state) => state.task
+  );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(setTodo([...todo, {title, description, priority,}]));
+    dispatch(setTitle(""));
+    dispatch(setDescription(""));
+    dispatch(setPriority(""));
+  };
+
+    const taskTitle = (event) => dispatch(setTitle(event.target.value));
+    const taskDescription = (event) => dispatch(setDescription(event.target.value));
+    const taskPriority = (event) => dispatch(setPriority(event.target.value));
+
+  return (
+    <section>
+      <form>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            className="ring ring-1 outline-0 rounded-sm"
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            placeholder="Title"
+            onChange={taskTitle}
+          />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="description">Description</label>
+          <input
+            className="ring ring-1 outline-0 rounded-sm"
+            type="text"
+            name="description"
+            id="description"
+            value={description}
+            placeholder="Description"
+            onChange={taskDescription}
+          />
+        </div>
+        <br />
+        <div>
+          <label htmlFor="priority">Priority</label>
+          <input
+            className="ring ring-1 outline-0 rounded-sm"
+            type="text"
+            name="priority"
+            id="priority"
+            value={priority}
+            placeholder="Priority"
+            onChange={taskPriority}
+          />
+        </div>
+        <br />
+        <div className="ml-10">
+          <button
+            type="submit"
+            className="h-10 w-fit px-3 bg-sky-600"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
 export default CreateTask;
