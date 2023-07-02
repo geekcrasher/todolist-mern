8270c16fca96c94535cc11f352d20e76120a819b
Paulo Lopez <geekcrasher@gmail.com>
2023-07-02 23:33:38 +0800

    renamed the action reducers

--- client/src/features/taskReducer.jsx
+++ client/src/features/taskReducer.jsx
@@ -1,29 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-   todo: [],
-   title: '',
-   description: '',
-   priority: ''
-}
+  todo: [
+   {  
+      title: "", 
+      description: "", 
+      priority: "" 
+   }
+],
+  title: "",
+  description: "",
+  priority: "",
+};
 
 const taskSlice = createSlice({
-   name: 'Task',
-   initialState,
-   reducers: {
-      taskTitle: (state, action) => {
-         state.title = action.payload
-      },
-      taskDescription: (state, action) => {
-         state.description = action.payload
-      },
-      taskPriority: (state, action) => {
-         state.priority = action.payload
-      }
-   }
-})
+  name: "Task",
+  initialState,
+  reducers: {
+    setTodo: (state, action) => {
+      state.todo = action.payload;
+    },
 
-const {taskTitle, taskDescription, taskPriority} = taskSlice.actions
+   //  setTodo: (state, action) => {
+   //    state.title = action.payload;
+   //    state.description = action.payload;
+   //    state.priority = action.payload;
+   //  }
 
-export default taskSlice.reducer
 
+    setTitle: (state, action) => {
+      state.title = action.payload;
+    },
+    setDescription: (state, action) => {
+      state.description = action.payload;
+    },
+    setPriority: (state, action) => {
+      state.priority = action.payload;
+    },
+  },
+});
+
+export const { setTodo, setTitle, setDescription, setPriority } = taskSlice.actions;
+
+export default taskSlice.reducer;
