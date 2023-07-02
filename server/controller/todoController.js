3a3a0c4a9c2bba92bbf122249f9dbb1833ee71de
Paulo Lopez <geekcrasher@gmail.com>
2023-07-02 23:58:23 +0800

    imported the todo model

--- server/controller/todoController.js
+++ server/controller/todoController.js
@@ -1,20 +1,31 @@
+const TodoModel = require('../models/todoModel')
+
 
 // get todo
 // route -> GET /api/todos
 const getTodo = async (req, res) => {
-   res.status(200).json({ msg: 'Get Tasks' })
+
+   try {
+      const response = await TodoModel.find()
+      console.log(response.data)
+   } catch (err) {
+      console.log(err)
+   }
+   // res.send('Hello')
+   // res.status(200).json({ msg: 'Get Tasks' })
 }
 
 // post todo
 // route -> POST /api/todos
 const addTodo = async (req, res) => {
-   res.status(200).json({ msg: 'Add Todo' })
+   // const {title, description, priority} = req.body 
+   res.status(200).send("Task created")
 }
 
 // update todo
 // route -> PUT /api/todos/:id
 const updateTodo = async (req, res) => {
-   res.status(200).json({ msg: 'Update Todo' })
+   res.status(200).send({ msg: 'Successfully updated the Task' })
 }
 
 // delete todo
