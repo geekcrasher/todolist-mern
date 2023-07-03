60deb0238d47069674950aca6704b333947ba468
Paulo Lopez <geekcrasher@gmail.com>
2023-07-03 14:45:46 +0800

    created a post request to send to db

--- server/controller/todoController.js
+++ server/controller/todoController.js
@@ -18,8 +18,13 @@ const getTodo = async (req, res) => {
 // post todo
 // route -> POST /api/todos
 const addTodo = async (req, res) => {
-   // const {title, description, priority} = req.body 
-   res.status(200).send("Task created")
+  try {
+   const response = await TodoModel.create(req.body)
+   res.json(response)
+   res.status(200).json("Task created")
+  } catch (error) {
+   res.status(500).json({message: error.message})
+  }
 }
 
 // update todo
