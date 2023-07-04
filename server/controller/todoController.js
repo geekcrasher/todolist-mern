4e0335bff9938c4c48e274135745709b8c9f2ca5
Paulo Lopez <geekcrasher@gmail.com>
2023-07-04 16:01:45 +0800

    add a delete and update controller and add validators to updateTask

--- server/controller/todoController.js
+++ server/controller/todoController.js
@@ -1,17 +1,24 @@
+
 const TodoModel = require("../models/todoModel");
 
-// get todo
-// route -> GET /api/todos
+
+/**
+ * get all tasks
+ * route -> GET /todo
+ */
 const getTasks = async (req, res) => {
   try {
     const response = await TodoModel.find();
-    res.status(200).json({ message: response });
+    res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-// get a single todo
+/**
+ * get a single task
+ * route -> GET /todos/:id
+ */
 const getTask = async (req, res) => {
    try {
       const { id } = req.params
@@ -20,31 +27,51 @@ const getTask = async (req, res) => {
       if(!response) {
          res.status(404).send(`No task exist with an id of ${id}`)
       }
-      res.status(200).json({ message: response });
+
+      res.status(200).json(response);
    } catch (error) {
       res.status(500).json({message: error.message})
    }
 }
 
-// post todo
-// route -> POST /api/todos
+/**
+ * post todo
+ * route -> POST /todos
+ */
 const addTask = async (req, res) => {
   try {
     const response = await TodoModel.create(req.body);
-    res.status(200).json({ message: response });
+    res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-// update todo
-// route -> PUT /api/todos/:id
+/**
+ * update todo
+ * route -> PUT /todos/:id
+ */
 const updateTask = async (req, res) => {
-  res.status(200).send({ msg: "Successfully updated the Task" });
+   try {
+      const { id } = req.params
+      const response = await TodoModel.findByIdAndUpdate(
+         {_id: id}, req.body, { new: true, runValidators: true}
+      )
+      
+      if (!response) {
+         res.status(404).json({ message: "No task found!" });
+      }
+
+      res.status(200).json(response)
+   } catch (error) {
+      res.status(500).json({message: error.message})
+   }
 };
 
-// delete todo
-// route -> DELETE /api/todos/:id
+/**
+ * delete todo
+ * route -> DELETE /todos/:id
+ */
 const deleteTask = async (req, res) => {
    try {
       const { id } = req.params
@@ -53,11 +80,13 @@ const deleteTask = async (req, res) => {
       if(!response) {
          res.status(404).send(`No task exist with an id of ${id}`)
       }
-      res.status(200).json({message: response})
+
+      res.status(200).send('Task successfully deleted')
    } catch (error) {
       res.status(500).json({message: error.message})
    }
 };
+
 
 module.exports = {
   getTasks,
