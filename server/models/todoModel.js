7617df8eb2ba4066c2f91e99052361838d8a64ed
Paulo Lopez <geekcrasher@gmail.com>
2023-07-04 15:58:20 +0800

    fix the wrong properties from require to required

--- server/models/todoModel.js
+++ server/models/todoModel.js
@@ -3,15 +3,15 @@ const mongoose = require('mongoose')
 const todoSchema = mongoose.Schema({
    title: {
       type: String,
-      require: [true, 'Please add a title task']
+      required: [true, 'Please add a title task']
    },
    description: {
       type: String,
-      require: [true, 'Please add a task description']
+      required: [true, 'Please add a task description']
    },
    priority: {
       type: String,
-      require: [true, 'Please add a level of priority']
+      required: [true, 'Please add a level of priority']
    },
 }, {
    timestamps: true
