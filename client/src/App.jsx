074aa55425349365e2b79f8cb2d081e5a925df45
Paulo Lopez <geekcrasher@gmail.com>
2023-07-06 00:51:31 +0800

    remove CreateTask and Task component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,32 @@
 import "./App.css";
-import CreateTask from "./components/CreateTask";
-import Task from "./components/Task";
+import { useState, useEffect } from "react";
+import Navbar from './components/Navbar'
+import TodoList from "./components/TodoList";
 
 function App() {
+
+   const [isClick, setIsClick] = useState(true)
+   const [theme, setTheme] = useState('light')
+
+   useEffect(() => {
+      if(theme === "dark") {
+         document.documentElement.classList.add("dark")
+      }
+      else {
+         document.documentElement.classList.remove("dark")
+      }
+
+   }, [theme])
+
+   const handleClick = () => {
+      setIsClick(!isClick)
+      setTheme(theme === "dark" ? "light" : "dark")
+   }
+
   return (
-    <main>
-      <CreateTask />
-      <Task />
+    <main className="min-h-screen w-full font-sans text-slate-100">
+      <Navbar onClick={handleClick} isClick={isClick}/>
+      <TodoList />
     </main>
   );
 }
