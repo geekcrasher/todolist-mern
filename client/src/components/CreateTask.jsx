6fd33be6ae0e0574b43617a05b7bde456fc594fa
Paulo Lopez <geekcrasher@gmail.com>
2023-07-06 00:53:30 +0800

    add  deleteTodo props for deleting a single task

--- client/src/components/CreateTask.jsx
+++ client/src/components/CreateTask.jsx
@@ -6,18 +6,23 @@ import {
   setPriority
 } from "../features/taskReducer";
 
-const CreateTask = () => {
+const CreateTask = ({addTodoMutation}) => {
+
   const dispatch = useDispatch();
 
   const {todo, title, description, priority } = useSelector(
     (state) => state.task
   );
 
+ const taskTitle = (event) => dispatch(setTitle(event.target.value));
+ const taskDescription = (event) => dispatch(setDescription(event.target.value));
+ const taskPriority = (event) => dispatch(setPriority(event.target.value));
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
     if(title, description, priority) {
-      dispatch(setTodo([...todo, {title, description, priority,}]));
+      dispatch(setTodo([...todo, addTodoMutation.mutate({title, description, priority})]));
       dispatch(setTitle(""));
       dispatch(setDescription(""));
       dispatch(setPriority(""));
@@ -27,15 +32,11 @@ const CreateTask = () => {
     }
   };
 
-    const taskTitle = (event) => dispatch(setTitle(event.target.value));
-    const taskDescription = (event) => dispatch(setDescription(event.target.value));
-    const taskPriority = (event) => dispatch(setPriority(event.target.value));
 
   return (
-    <section>
-      <form>
+      <form className="mt-24" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="title">Title</label>
+          <label htmlFor="title" className="inline-block w-24 text-black">Title</label>
           <input
             className="ring-1 outline-0 rounded-sm"
             type="text"
@@ -48,7 +49,7 @@ const CreateTask = () => {
         </div>
         <br />
         <div>
-          <label htmlFor="description">Description</label>
+          <label htmlFor="description" className="inline-block w-24 text-black">Description</label>
           <input
             className="ring-1 outline-0 rounded-sm"
             type="text"
@@ -61,7 +62,7 @@ const CreateTask = () => {
         </div>
         <br />
         <div>
-          <label htmlFor="priority">Priority</label>
+          <label htmlFor="priority" className="inline-block w-24 text-black">Priority</label>
           <input
             className="ring-1 outline-0 rounded-sm"
             type="text"
@@ -77,13 +78,11 @@ const CreateTask = () => {
           <button
             type="submit"
             className="h-10 w-fit px-3 bg-sky-600"
-            onClick={handleSubmit}
           >
             Submit
           </button>
         </div>
       </form>
-    </section>
   );
 };
 
