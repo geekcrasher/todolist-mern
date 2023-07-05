1d8fd7d91f9640321e3dde4d094426a03caa00ea
Paulo Lopez <geekcrasher@gmail.com>
2023-07-06 00:52:30 +0800

    created request methods for the todolist

--- client/src/api/apiHandler.js
+++ client/src/api/apiHandler.js
@@ -1,6 +1,38 @@
 import axios from "axios";
 
-export const api = axios.create({
+const api =  axios.create({
+   // baseURL: 'http://localhost:4000',
+
    baseURL: import.meta.env.VITE_API_BASE_URL,
    headers: { "Access-Control-Allow-Origin": "*" },
 })
+
+// get all the tasks
+export const getTasks = async () => {
+   // const response = await api.get('/todos')  
+   return await api.get('/todos')
+}
+
+// get a single task
+export const getTask = async (id) => {
+   const response = await api.get(`/todos/${id}`)
+   return response.data
+}
+
+// add a task
+export const addTask = async (todo ) => {
+   return await api.post('/todos', todo)
+}
+
+// update a task
+export const updateTask = async (todo) => {
+   return await api.put(`/todos/${todo.id}`, todo)
+}
+
+// delete a task
+export const deleteTask = async (id) => {
+   return await api.delete(`/todos/${id}`, id)
+}
+
+
+export default api;
