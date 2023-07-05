6dba2b36aba8d3ccd517ada545e91bcdde95b370
Paulo Lopez <geekcrasher@gmail.com>
2023-07-06 01:07:04 +0800

    imported the QueryClientProvider from tanstack/react-query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "./features/taskStore.js";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
