749e678b5864cd7b7ec53be04685369354e35b74
Paulo Lopez <geekcrasher@gmail.com>
2023-07-05 21:23:07 +0800

    component for a single task

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,18 +1,15 @@
-import { useSelector } from "react-redux";
 
-const Task = () => {
-  const { todo } = useSelector((state) => state.task);
-
+const Task = ({todos}) => {
   return (
-    <section>
-      {todo.map((task, key) => {
-        const { id, title, description, priority } = task;
+    <section className="border p-3">
+      {todos?.data?.map((task, key) => {
+        const { _id, title, description, priority } = task;
         return (
           <section
             key={key}
-            className="border rounded-md py-3 px-4 bg-orange-200"
+            className="border rounded-md py-3 px-4 bg-slate-800"
           >
-            <h1>{id}</h1>
+            <h1>{_id}</h1>
             <h1>{title}</h1>
             <h1>{description}</h1>
             <h1>{priority}</h1>
