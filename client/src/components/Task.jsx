4e5904c85715a28363fb1fd9c737a7f175b3066d
Paulo Lopez <geekcrasher@gmail.com>
2023-07-08 21:27:17 +0800

    imported the Modal component

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
+import {Modal} from "./Modal";
 
 const Task = ({ todos, deleteTodoMutation, updateTodoMutation }) => {
 
@@ -19,20 +20,24 @@ const Task = ({ todos, deleteTodoMutation, updateTodoMutation }) => {
    }))
   }
 
+  const deleteTask = (id) => {
+   deleteTodoMutation.mutate(id)
+  }
+
   const saveUpdatedTask = () => {
    updateTodoMutation.mutate(updatedTask)
   }
 
   return (
-    <section className="border p-3">
+    <section className=" col-span-3 w-full">
+      <section className="grid grid-cols-3 gap-3 col-span-3 w-full">
       {todos?.data?.map((todo) => {
         const { _id, title, description, priority } = todo;
         return (
           <section
             key={_id}
-            className="border rounded-md py-3 px-4 bg-slate-800"
+            className="rounded-md py-3 px-4 bg-slate-800"
           >
-            <h1>{_id}</h1>
             <h1>{title}</h1>
             <h1>{description}</h1>
             <h1>{priority}</h1>
@@ -46,7 +51,7 @@ const Task = ({ todos, deleteTodoMutation, updateTodoMutation }) => {
                 Update
               </button>
               <button
-                onClick={() => deleteTodoMutation.mutate(_id)}
+                onClick={() => deleteTask(_id)}
                 className="bg-sky-700 py-1 px-4"
               >
                 Delete
@@ -56,50 +61,11 @@ const Task = ({ todos, deleteTodoMutation, updateTodoMutation }) => {
         );
       })}
       {/* update modal */}
-      <section>
-        <dialog id="my_modal_1" className="modal">
-          <form method="dialog" className="modal-box">
-            <h1 className="font-semibold text-2xl">Update a Task</h1>
-            <br />
-            <div>
-              <label htmlFor="title" className="label">Title</label>
-              <input
-                type="text"
-                placeholder="Title"
-                className="input w-full max-w-xs mb-2 border-slate-300"
-                name="title"
-                value={updatedTask.title ? updatedTask.title : ''}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="description" className="label"> Description</label>
-              <input
-                type="text"
-                placeholder="Description"
-                className="input w-full max-w-xs mb-2 border-slate-300"
-                name="description"
-                value={updatedTask.description ? updatedTask.description : ''}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label htmlFor="priority" className="label">Priority</label>
-              <input
-                type="text"
-                placeholder="Priority"
-                className="input w-full max-w-xs mb-2 border-slate-300"
-                name="priority"
-                value={updatedTask.priority ? updatedTask.priority : ''}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="modal-action">
-              <button className="btn">Cancel</button>
-              <button className="btn" onClick={() =>  updateTodoMutation.mutate(updatedTask)}>Save Changes</button>
-            </div>
-          </form>
-        </dialog>
+      <Modal 
+         updatedTask={updatedTask} 
+         handleChange={handleChange} 
+         saveUpdatedTask={saveUpdatedTask} 
+      />
       </section>
     </section>
   );
@@ -110,4 +76,5 @@ export default Task;
 Task.propTypes = {
   todos: PropTypes.object,
   deleteTodoMutation: PropTypes.object,
+  updateTodoMutation: PropTypes.object,
 };
