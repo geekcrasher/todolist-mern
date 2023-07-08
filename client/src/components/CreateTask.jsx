c5b3fd1c57a331f8477a8dd1dee032f65710011e
Paulo Lopez <geekcrasher@gmail.com>
2023-07-08 21:30:23 +0800

    change the style of the form element

--- client/src/components/CreateTask.jsx
+++ client/src/components/CreateTask.jsx
@@ -7,13 +7,15 @@ import {
 } from "../features/taskReducer";
 
 const CreateTask = ({ addTodoMutation }) => {
+  const dispatch = useDispatch();
+  const { todo, title, description, priority } = useSelector(
+    (state) => state.task
+  );
 
-   const dispatch = useDispatch();
-   const { todo, title, description, priority } = useSelector((state) => state.task);
-
-   const taskTitle = (event) => dispatch(setTitle(event.target.value));
-   const taskDescription = (event) => dispatch(setDescription(event.target.value));
-   const taskPriority = (event) => dispatch(setPriority(event.target.value));
+  const taskTitle = (event) => dispatch(setTitle(event.target.value));
+  const taskDescription = (event) =>
+    dispatch(setDescription(event.target.value));
+  const taskPriority = (event) => dispatch(setPriority(event.target.value));
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -33,59 +35,71 @@ const CreateTask = ({ addTodoMutation }) => {
     }
   };
 
+  const cancel = () => {
+      dispatch(setTitle(""));
+      dispatch(setDescription(""));
+      dispatch(setPriority(""));
+  }
+
+
   return (
-    <form className="mt-24" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor={title} className="label inline-block w-24 ">
-          Title
-        </label>
-        <input
-          className="input border-slate-300"
-          type="text"
-          name="title"
-          id="title"
-          value={title}
-          placeholder="Title"
-          onChange={taskTitle}
-        />
-      </div>
-      <br />
-      <div>
-        <label htmlFor={description} className="label inline-block w-24 ">
-          Description
-        </label>
-        <input
-          className="input border-slate-300"
-          type="text"
-          name="description"
-          id="description"
-          value={description}
-          placeholder="Description"
-          onChange={taskDescription}
-        />
-      </div>
-      <br />
-      <div>
-        <label htmlFor={priority} className=" label inline-block w-24 ">
-          Priority
-        </label>
-        <input
-          className="input border-slate-300"
-          type="text"
-          name="priority"
-          id="priority"
-          value={priority}
-          placeholder="Priority"
-          onChange={taskPriority}
-        />
-      </div>
-      <br />
-      <div className="ml-10">
-        <button type="submit" className="h-10 w-fit px-3 bg-sky-600">
-          Submit
-        </button>
-      </div>
-    </form>
+    <section className="card bg-[#1D232A] grid col-span-1 w-full">
+      <form className="card-body form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor={title} className="label">
+            <span className="label-text">Title</span>
+          </label>
+          <input
+            className="input border-[#42454a] w-full max-w-xs"
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            placeholder="e.g. Todo App Project"
+            onChange={taskTitle}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor={description} className="label">
+            <span className="label-text">Description</span>
+          </label>
+          <input
+            className="input border-[#42454a] truncate w-full max-w-xs"
+            type="text"
+            name="description"
+            id="description"
+            value={description}
+            placeholder="Develop a new feature for the web application"
+            onChange={taskDescription}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor={priority} className=" label">
+            <span className="label-text">Priority</span>
+          </label>
+          <input
+            className="input border-[#42454a] w-full max-w-xs"
+            type="text"
+            name="priority"
+            id="priority"
+            value={priority}
+            placeholder="Medium"
+            onChange={taskPriority}
+          />
+          <label className="label">
+            <span className="label-text-alt">Low, Medium, High</span>
+          </label>
+        </div>
+        <div className="mt-6 grid grid-cols-2 gap-1 w-full">
+          <button type="reset" className="btn bg-[#29323C]" onClick={cancel}>
+            Cancel
+          </button>
+          <button type="submit" className="btn bg-[#29323C]">
+            Create task
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
