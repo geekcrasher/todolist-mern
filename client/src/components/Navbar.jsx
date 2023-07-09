f0d619aa312cdf2ad1d2f888ff94b422c5ada9ac
Paulo Lopez <geekcrasher@gmail.com>
2023-07-09 17:27:22 +0800

    remove unnecessary elements

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,16 +6,14 @@ const Navbar = ({ onClick, isClick }) => {
 
 
   return (
-    <nav className=" h-24 flex items-center justify-between fixed top-0 z-20 w-full  xs:px-6 md:px-10 bg-[#191E24]">
-      {/* <section className="flex items-center cursor-pointer"> */}
-        <NavLink className="btn hover:bg-transparent border-none">
-         <span><ListTodo color="#A6ADB9" size={28} /></span>
-         <span className="font-figtree lg:text-2xl ml-2 text-[#A6ADB9] lowercase">
-            task
-            <span className="font-figtree lg:text-2xl text-[#00D1A7] normal-case">Tracker</span>
-         </span>
+    <nav className=" h-24 flex items-center justify-between fixed top-0 z-20 w-full  xs:px-6 md:px-10 dark:bg-[#191E24]">
+        <NavLink to={"/todo"} className="flex items-center cursor-pointer">
+            <span><ListTodo color="#A6ADB9" size={28} /></span>
+            <span className="font-figtree lg:text-2xl ml-2 text-[#1E2936] dark:text-[#A6ADB9] lowercase">
+               task
+               <span className="font-figtree lg:text-2xl text-[#00D1A7] normal-case">Tracker</span>
+            </span>
         </NavLink>
-      {/* </section> */}
       <section className="mt-1">
           <button onClick={onClick} className=" hover:bg-transparent border-none p-1 rounded-md active:motion-safe:animate-spin">
             {isClick ? <MoonStar color="#A6ADB9" size={22}/> : <Sun color="#A6ADB9" />}
