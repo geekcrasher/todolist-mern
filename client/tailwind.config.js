492cf94bdd0a63e976f08d6ced36471de1273fcd
Paulo Lopez <geekcrasher@gmail.com>
2023-07-12 23:37:04 +0800

    added a new screen breakpoinst

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,40 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-   "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-   extend: {
-     screens: {
-       xs: "360px",
-       sm: "480px",
-       md: "547px",
-       lg: "768px",
-       xl: "1024px",
-       mxl: "1352px",
-       xxl: "1680px",
-     },
-     fontFamily: {
-       "noto-sans": ["Noto Sans", "sans-serif"],
-       figtree: ["Figtree", "sans-serif"]
-     },
-     width: {
-       "12/12": "95%",
-     },
-     backgroundColor: {
-      main_dark: "#191E24",
-      pink_primary: "#F86465",
-      pink_secondary: "#c94f51",
-      priority: "#313136",
-      task: "#F6F6F7",
-      task_dark: "#252529",
-      highlight: "#e2e2e3"
+    extend: {
+      screens: {
+        xxs: "360px",
+        xs: "480px",
+        sm: "547px",
+        md: "640px",
+        lg: "768px",
+        xl: "1024px",
+        mxl: "1352px",
+        xxl: "1680px",
+      },
+      fontFamily: {
+        "noto-sans": ["Noto Sans", "sans-serif"],
+        figtree: ["Figtree", "sans-serif"],
+      },
+      width: {
+        "12/12": "95%",
+      },
+      backgroundColor: {
+        main_dark: "#191E24",
+        pink_primary: "#F86465",
+        pink_secondary: "#c94f51",
+        priority: "#313136",
+        task: "#F6F6F7",
+        task_dark: "#252529",
+        highlight: "#e2e2e3",
+      },
     },
-   },
   },
-//   require("daisyui")
+  //   require("daisyui")
   plugins: [require("daisyui")],
-}
-
+};
