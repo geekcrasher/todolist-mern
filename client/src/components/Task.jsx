dc280d0e734ba207da2f83aa0768ca4f9ee51fe0
Paulo Lopez <geekcrasher@gmail.com>
2023-07-12 23:33:49 +0800

    set a breakpoints to make the Task component responsive to any device

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
-import { Trash2, Edit3, MoreHorizontal } from "lucide-react";
+import { Trash2, Edit3, MoreHorizontal, Plus } from "lucide-react";
 import { Modal } from "./Modal";
+import CreateTask from "./CreateTask";
 
-const Task = ({ todos, deleteTodoMutation, updateTodoMutation }) => {
+const Task = ({
+  todos,
+  addTodoMutation,
+  updateTodoMutation,
+  deleteTodoMutation,
+}) => {
   const [updatedTask, setUpdatedTask] = useState({});
 
   const updateTask = (todo) => {
@@ -29,49 +35,69 @@ const Task = ({ todos, deleteTodoMutation, updateTodoMutation }) => {
   };
 
   const created = (created) => {
-   const month = new Date(created).getMonth() + 1
-   const date = new Date(created).getDate()
-   const year = new Date(created).getFullYear()
-   return `${month}/${date}/${year}`
-  }
+    const month = new Date(created).getMonth() + 1;
+    const date = new Date(created).getDate();
+    const year = new Date(created).getFullYear();
+    return `${month}/${date}/${year}`;
+  };
+
+  const addNewTask = () => {
+    return window.my_modal_2.showModal();
+  };
 
   return (
-    <section className="relative  col-span-3 w-full">
-      <div className="mb-4">
-        <h1 className="font-figtree text-xl text-[#A6ADB9]">Your todos</h1>
-      </div>
-      <section className="grid grid-cols-3 gap-3 col-span-3 w-full">
+    <section className="w-fit ">
+      <section className="flex justify-between items-center">
+        <h1 className="font-figtree lg:text-xl text-[#A6ADB9]">Your todos</h1>
+        <button className="xxs:w-28 md:w-32 h-10 flex justify-evenly items-center rounded-lg bg-[#2fa199] hover:bg-[#058e85] active:bg-[#2fa199] normal-case text-xs md:text-sm text-white" onClick={addNewTask}>
+          <span>
+            <Plus size={18} color="#f8fafc"/>
+          </span>
+          <span className="font-sans font-semibold text-xs lg:text-sm">Create task</span>
+        </button>
+      </section>
+
+      <section className=" grid sm:grid-cols-1 lg:grid-cols-2 mxl:grid-cols-3 xxl:grid-cols-4 gap-6 w-fit mt-14">
         {todos?.data?.map((todo) => {
           const { _id, title, description, priority, createdAt } = todo;
           return (
             <section
               key={_id}
-              className="card relative rounded-md h-66 p-6 bg-[#1D232A]"
+              className=" card relative w-full rounded-md h-60 py-4 px-5 bg-[#1D232A]"
             >
-              <div className=" overflow-hidden p-0 w-[19rem] h-14 mb-2">
-                <h1 className="break-words hyphens-manual card-title font-figtree text-[1.125rem] text-[#058e85]">
+              <div className="overflow-hidden p-0 w-[19rem] mxl:w-64 h-14 mb-2 ">
+                <h1 className="break-words hyphens-manual card-title font-figtree text-[0.98rem] mxl:text-[0.94rem] text-[#058e85]">
                   {title}
                 </h1>
               </div>
-              <div className="h-28 ">
-                <p className="hyphens-manual text-[0.9rem] text-[#7f848a] ">
+              <div className="h-28 py-1 mb-3 ">
+                <p className="tracking-wide hyphens-manual text-[0.78rem] mxl:text-[0.82rem] text-[#6a6e73]">
                   {description}
                 </p>
               </div>
-              <div className="flex justify-between items-center mt-3 ">
-                  <div className="badge badge-neutral">
-                     <h1 className="text-xs">{priority}</h1>
-                  </div>
-                  <span className="text-xs text-[#7f848a]">{created(createdAt)}</span>
+              <div className="flex justify-between items-center ">
+                <div className="badge badge-neutral">
+                  <h1 className="text-xs">{priority}</h1>
+                </div>
+                <span className="text-xs text-[#7f848a]">
+                  {created(createdAt)}
+                </span>
               </div>
-              <section className="absolute top-7 right-7">
+              <section className="absolute top-5 right-7">
                 <div className="dropdown dropdown-bottom dropdown-end">
                   <button tabIndex={0} className="">
-                    <MoreHorizontal size={20} color="#A6ADB9" />{" "}
+                    <MoreHorizontal size={16} color="#A6ADB9" />
                   </button>
-                  <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box"
+                  >
                     <li>
-                      <button onClick={() => {updateTask(todo)}}>
+                      <button
+                        onClick={() => {
+                          updateTask(todo);
+                        }}
+                      >
                         <span>
                           <Edit3 size={14} />
                         </span>
@@ -92,12 +118,17 @@ const Task = ({ todos, deleteTodoMutation, updateTodoMutation }) => {
             </section>
           );
         })}
+      </section>
+      <>
+        {/* update modal */}
+        <CreateTask addTodoMutation={addTodoMutation} />
+        {/* update modal */}
         <Modal
           updatedTask={updatedTask}
           handleChange={handleChange}
           saveUpdatedTask={saveUpdatedTask}
         />
-      </section>
+      </>
     </section>
   );
 };
