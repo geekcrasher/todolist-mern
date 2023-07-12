37e88e78a774a6c5a8ce3cf3773c8e1fc2b7dc30
Paulo Lopez <geekcrasher@gmail.com>
2023-07-12 23:30:32 +0800

    added padding horizontally

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,16 +6,16 @@ const Navbar = ({ onClick, isClick }) => {
 
 
   return (
-    <nav className=" h-24 flex items-center justify-between fixed top-0 z-20 w-full  xs:px-6 md:px-10 dark:bg-[#191E24]">
-        <NavLink to={"/todo"} className="flex items-center cursor-pointer">
-            <span><ListTodo color="#A6ADB9" size={28} /></span>
-            <span className="font-figtree lg:text-2xl ml-2 text-[#1E2936] dark:text-[#A6ADB9] lowercase">
-               task
-               <span className="font-figtree lg:text-2xl text-[#00D1A7] normal-case">Tracker</span>
-            </span>
-        </NavLink>
-      <section className="mt-1">
-          <button onClick={onClick} className=" hover:bg-transparent border-none p-1 rounded-md active:motion-safe:animate-spin">
+    <nav className=" h-24 flex items-center justify-between fixed top-0 z-20 w-full px-6 dark:bg-[#191E24]">
+      <NavLink to={"/todo"} className="flex items-center cursor-pointer">
+         <span><ListTodo color="#A6ADB9" size={24} /></span>
+         <span className="font-figtree lg:text-xl ml-2 text-[#1E2936] dark:text-[#A6ADB9] lowercase">
+            Task
+         </span>
+         <span className="font-figtree lg:text-xl text-[#00D1A7] normal-case">Tracker</span>
+      </NavLink>
+      <section className="flex items-center ">
+          <button onClick={onClick} className=" hover:bg-transparent border-none rounded-md active:motion-safe:animate-spin lowercase">
             {isClick ? <MoonStar color="#A6ADB9" size={22}/> : <Sun color="#A6ADB9" />}
           </button>
       </section>
