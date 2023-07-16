32706c314840e79c583d2dd2b640196e4156a7d3
Paulo Lopez <geekcrasher@gmail.com>
2023-07-16 17:27:19 +0800

    removed the useEffect and useState hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,15 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import Footer from "./components/Footer";
 import Navbar from './components/Navbar'
 import TodoList from "./components/TodoList";
 import { Route, Router, Routes } from "react-router-dom";
 
 function App() {
-
-   const [isClick, setIsClick] = useState(true)
-   const [theme, setTheme] = useState('light')
-
-   useEffect(() => {
-      if(theme === "dark") {
-         document.documentElement.classList.add("dark")
-      }
-      else {
-         document.documentElement.classList.remove("dark")
-      }
-
-   }, [theme])
-
-   const handleClick = () => {
-      setIsClick(!isClick)
-      setTheme(theme === "dark" ? "light" : "dark")
-   }
-
   return (
-    <main className="min-h-screen w-full bg-white dark:bg-main_dark grid place-items-center relative">
-      <Navbar onClick={handleClick} isClick={isClick}/>
+    <main className="min-h-screen w-full relative flex justify-center">
+      <Navbar />
       <TodoList />
+      <Footer />
     </main>
   );
 }
