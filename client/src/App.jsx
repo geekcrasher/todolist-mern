6b41f21e76120f2b83b193139f387dd7284749b7
Paulo Lopez <geekcrasher@gmail.com>
2023-07-19 01:38:05 +0800

    fixed the correct ditectory of TodoList component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 import Header from './components/Header'
-import TodoList from "./components/TodoList";
+import TodoList from "./components/TodoList/TodoList";
 import { Route, Router, Routes } from "react-router-dom";
 
 function App() {
