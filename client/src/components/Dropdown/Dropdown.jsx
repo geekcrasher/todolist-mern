dff3c1a210a2f2914c8bec2663e12511e9bead57
Paulo Lopez <geekcrasher@gmail.com>
2023-07-21 19:26:13 +0800

    changed the background color to white

--- client/src/components/Dropdown/Dropdown.jsx
+++ client/src/components/Dropdown/Dropdown.jsx
@@ -1,39 +1,43 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import { Trash2, Edit3, MoreHorizontal } from "lucide-react";
 
-const Dropdown = ({updateTask, deleteTask, todo, _id}) => {
+const Dropdown = ({ setUpdatedTask, deleteTodoMutation, todo, _id }) => {
+
+  const updateTask = (todo) => {
+    setUpdatedTask(todo);
+    window.my_modal_1.showModal();
+  };
+
+  const deleteTask = (id) => {
+    deleteTodoMutation.mutate(id);
+  };
+
   return (
-    <section className="card-actions absolute top-5 right-7">
-      <section className="dropdown dropdown-bottom dropdown-end">
-        <button tabIndex={0} className="">
-          <MoreHorizontal size={16} color="#A6ADB9" />
-        </button>
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box"
-        >
-          <li>
-            <button
-              onClick={() => {
-                updateTask(todo);
-              }}
-            >
-              <span>
-                <Edit3 size={14} />
-              </span>
-              <span>Update</span>
-            </button>
-          </li>
-          <li>
-            <button onClick={() => deleteTask(_id)}>
-              <span>
-                <Trash2 size={14} />
-              </span>
-              <span>Delete</span>
-            </button>
-          </li>
-        </ul>
-      </section>
+    <section className="flex items-center p-1 card-actions absolute top-5 right-5 dropdown dropdown-bottom dropdown-end rounded-full">
+      <button tabIndex={0} className="btn btn-sm btn-circle btn-ghost">
+        <MoreHorizontal size={16} color="#A6ADB9" />
+      </button>
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] menu p-2 shadow rounded-box bg-white"
+      >
+        <li className="text-slate-900">
+          <button className="hover:text-slate-900" onClick={() => updateTask(todo)}>
+            <span>
+              <Edit3 size={14} />
+            </span>
+            <span>Update</span>
+          </button>
+        </li>
+        <li className="text-slate-900">
+          <button className="hover:text-slate-900" onClick={() => deleteTask(_id)}>
+            <span>
+              <Trash2 size={14} />
+            </span>
+            <span>Delete</span>
+          </button>
+        </li>
+      </ul>
     </section>
   );
 };
@@ -41,8 +45,8 @@ const Dropdown = ({updateTask, deleteTask, todo, _id}) => {
 export default Dropdown;
 
 Dropdown.propTypes = {
-   updateTask: PropTypes.func, 
-   deleteTask: PropTypes.func, 
-   todo: PropTypes.object, 
-   _id: PropTypes.string
-}
+  setUpdatedTask: PropTypes.func,
+  deleteTodoMutation: PropTypes.object,
+  todo: PropTypes.object,
+  _id: PropTypes.string,
+};
