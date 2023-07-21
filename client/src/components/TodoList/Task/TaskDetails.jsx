626d30774e5b74ef864adac794635a8d5d1eb9e1
Paulo Lopez <geekcrasher@gmail.com>
2023-07-21 19:22:07 +0800

    removed the unnecessary parent element and change the section to article element

--- client/src/components/TodoList/Task/TaskDetails.jsx
+++ client/src/components/TodoList/Task/TaskDetails.jsx
@@ -1,33 +1,55 @@
 import PropTypes from 'prop-types'
+import { ChevronsUp, CalendarDays } from 'lucide-react'; 
 
-const TaskDetails = ({title, description, priority, createdAt, created}) => {
+const TaskDetails = ({
+   title, 
+   description, 
+   priority, 
+   createdAt,
+   priorityType 
+}) => {
+
+   const created = (created) => {
+      const month = new Date(created).getMonth() + 1;
+      const date = new Date(created).getDate();
+      const year = new Date(created).getFullYear();
+      return `${month}/${date}/${year}`;
+    };
+
   return (
-    <section className="card-body py-3.5 px-5">
-      <section className="break-words  overflow-hidden p-0 w-11/12 h-14">
-        <h1 className="hyphens-manual card-title font-figtree text-[0.88rem] xl:text-[0.90rem] text-[#058e85]">
+    <article className="h-full relative ">
+      <section className="break-words overflow-hidden h-12 w-72">
+        <h1 className="font-figtree text-[0.88rem] xl:text-[1rem] text-[#333]">
           {title}
         </h1>
       </section>
-      <section className="h-24 mt-2 py-1">
-        <p className="tracking-wide line-clamp-4 text-[0.78rem] xs:text-[0.79rem] text-[#7f848a]">
+      <section className="mt-2  h-fit py-1">
+        <p className="leading-normal tracking-wide line-clamp-4 text-[0.78rem] xs:text-[0.79rem] text-[#333]">
           {description}
         </p>
       </section>
-      <section className="flex items-center justify-between mt-1">
-        <p className="text-xs text-[#7f848a] font-semibold">{priority}</p>
-        <span className="text-xs text-[#7f848a]">{created(createdAt)}</span>
+
+      <section className=" w-full flex items-center justify-between absolute bottom-0 p-1">
+        <p className={`${priorityType(priority)} flex items-center text-xs font-semibold py-0.5 px-2 rounded-lg`}>
+            <span className='mr-1'><ChevronsUp size={14}/></span>
+            {priority}
+         </p>
+         <span className=" flex items-center text-xs text-[#333]">
+            <span className='mr-1'><CalendarDays size={12}/></span>
+            {created(createdAt)}
+         </span>
       </section>
-    </section>
+    </article>
   );
 };
 
 export default TaskDetails;
 
-
 TaskDetails.propTypes = {
+   _id: PropTypes.string,
    title: PropTypes.string,
    description: PropTypes.string,
    priority: PropTypes.string,
    createdAt: PropTypes.string,
-   created: PropTypes.func,
+   priorityType: PropTypes.func,
 }
