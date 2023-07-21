844257aa5926856def094e77de2a04e5e7938df8
Paulo Lopez <geekcrasher@gmail.com>
2023-07-21 19:20:28 +0800

    changed the minor details in the component and removed the update and delete function

--- client/src/components/TodoList/Task/Tasks.jsx
+++ client/src/components/TodoList/Task/Tasks.jsx
@@ -2,90 +2,78 @@ import PropTypes from "prop-types";
 import { useState } from "react";
 import UpdateTaskModal from "../../Modal/UpdateTaskModal";
 import CreateTaskModal from "../../Modal/CreateTaskModal";
-import Dropdown from "../../Dropdown/Dropdown";
 import TaskDetails from "./TaskDetails";
+import Dropdown from "../../Dropdown/Dropdown";
 
-const Task = ({
+const Tasks = ({
   todos,
   addTodoMutation,
   updateTodoMutation,
   deleteTodoMutation,
 }) => {
+
   const [updatedTask, setUpdatedTask] = useState({});
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setUpdatedTask((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const updateTask = (todo) => {
-   setUpdatedTask(todo);
-   window.my_modal_1.showModal();
-  };
-
-  const deleteTask = (id) => {
-    deleteTodoMutation.mutate(id);
-  };
 
   const saveUpdatedTask = () => {
     updateTodoMutation.mutate(updatedTask);
   };
 
-  const created = (created) => {
-    const month = new Date(created).getMonth() + 1;
-    const date = new Date(created).getDate();
-    const year = new Date(created).getFullYear();
-    return `${month}/${date}/${year}`;
-  };
+  const priorityType = (priority) => {
+   if(priority == 'Low') {
+      return 'bg-[#FFEFE0] text-[#F69547]'
+   }
+   else if (priority == 'Medium') {
+      return 'bg-[#ECF2FE] text-[#1974FB]'
+   }
+   else if (priority == 'High') {
+      return 'bg-[#F1ECFE] text-[#6B3DE0]'
+   }
+  }
 
   return (
-    <section className="px-4 mt-10">
+    <section className=" mt-10 mb-10">
       <section className="grid sm:grid-cols-1 lg:grid-cols-2 mxl:grid-cols-3 xxl:grid-cols-4 gap-6">
         {todos?.data?.map((todo) => {
           const { _id, title, description, priority, createdAt } = todo;
           return (
-            <section key={_id} className="card  rounded-md h-[14rem] bg-neutral">
-               <TaskDetails 
-                  title={title} 
-                  description={description} 
-                  priority={priority}
-                  created={created} 
-                  createdAt={createdAt}
-               />
+            <section key={_id} className="bg-white h-56 shadow relative rounded-xl overflow-hidden px-5 pt-7 pb-3">
+               {/* Task details */}
+              <TaskDetails
+                title={title}
+                description={description}
+                priority={priority}
+                createdAt={createdAt}
+                priorityType={priorityType}
+              />
 
-               {/* dropdown for update and delete action */}
-               <Dropdown 
-                  updateTask={updateTask} 
-                  deleteTask={deleteTask} 
-                  todo={todo} 
-                  _id={_id}
-               />
+              {/* dropdown for update and delete action */}
+              <Dropdown
+                _id={_id}
+                setUpdatedTask={setUpdatedTask}
+                deleteTodoMutation={deleteTodoMutation}
+                todo={todo}
+              />
             </section>
           );
         })}
       </section>
-      <>
-        {/* update modal */}
-        <CreateTaskModal addTodoMutation={addTodoMutation} />
 
-        {/* update modal */}
-        <UpdateTaskModal
-          updatedTask={updatedTask}
-          handleChange={handleChange}
-          saveUpdatedTask={saveUpdatedTask}
-        />
-      </>
+      {/* update modal */}
+      <CreateTaskModal addTodoMutation={addTodoMutation} />
+
+      {/* update modal */}
+      <UpdateTaskModal
+        updatedTask={updatedTask}
+        saveUpdatedTask={saveUpdatedTask}
+        setUpdatedTask={setUpdatedTask}
+      />
     </section>
   );
 };
 
-export default Task;
+export default Tasks;
 
-Task.propTypes = {
+Tasks.propTypes = {
   todos: PropTypes.object,
   addTodoMutation: PropTypes.object,
   deleteTodoMutation: PropTypes.object,
