5cf0d6f5864228a628d2d2722c705a6c101c9b9d
Paulo Lopez <geekcrasher@gmail.com>
2023-07-23 01:19:53 +0800

    added a todos props for counting the number of current todos

--- client/src/components/TodoList/Header/TodoListHeader.jsx
+++ client/src/components/TodoList/Header/TodoListHeader.jsx
@@ -1,13 +1,18 @@
 import { Plus } from "lucide-react";
 
-const TodoListHeader = () => {
+const TodoListHeader = ({todos}) => {
   const addNewTask = () => {
     return window.my_modal_2.showModal();
   };
 
   return (
-    <section className="flex justify-between items-center">
-      <h1 className="font-figtree lg:text-lg text-black">Your todos</h1>
+    <section className="flex justify-between items-center py-1">
+      <section className="w-32 flex justify-between items-center lg:text-lg">
+      <h1 className="font-figtree text-[#333]">
+         Your todos: <span className="text-gray-400">{todos?.data.length}</span>
+      </h1>
+      {/* <h1 className="font-nunito text-slate-600">{todos?.data.length}</h1> */}
+      </section>
       <button
         className="w-10 sm:w-28 lg:w-32 h-10 flex justify-evenly outline-0 items-center rounded-lg bg-[#2fa199] hover:bg-[#058e85] active:bg-[#2fa199] normal-case text-xs md:text-sm text-white"
         onClick={addNewTask}
