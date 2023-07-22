3f150cd2e99aa8097bec7de0b5ef355def309d6a
Paulo Lopez <geekcrasher@gmail.com>
2023-07-23 01:14:09 +0800

    created a separated between personal icon and github icon

--- client/src/components/Footer/Contact/Contact.jsx
+++ client/src/components/Footer/Contact/Contact.jsx
@@ -3,10 +3,11 @@ import { Github, Linkedin, Facebook  } from "lucide-react";
 
 const Contact = () => {
    return ( 
-      <section className="flex items-center justify-between w-fit space-x-3">
-         <Link to={"#"}>
+      <section className="flex items-center justify-between w-fit space-x-2">
+        <Link to={"https://github.com/geekcrasher"} target="_blank">
             <img src="./personal-icon.svg" alt="personal icon" className="w-6 h-auto border-0 rounded-full overflow-hidden outline-none"/>
          </Link>
+         <section className="w-0.5 h-5  bg-gray-400"></section>
          <Link to={"https://github.com/geekcrasher"} target="_blank">
             <Github size={18} color="#6b7280"/>
          </Link>
