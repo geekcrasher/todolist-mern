7d85f117ac0fb22f6329f68e57c5e266595a24a8
Paulo Lopez <geekcrasher@gmail.com>
2023-07-23 23:53:12 +0800

    removed the unnecessary tailwind css properties

--- client/src/components/TodoList/Task/TaskDetails.jsx
+++ client/src/components/TodoList/Task/TaskDetails.jsx
@@ -1,43 +1,72 @@
-import PropTypes from 'prop-types'
-import { ChevronsUp, CalendarDays } from 'lucide-react'; 
+import PropTypes from "prop-types";
+import { ChevronsUp, Calendar } from "lucide-react";
+import Dropdown from "../../Dropdown/Dropdown";
 
 const TaskDetails = ({
-   title, 
-   description, 
-   priority, 
-   createdAt,
-   priorityType 
+  _id,
+  title,
+  description,
+  priority,
+  createdAt,
+  setUpdatedTask,
+  deleteTodoMutation,
+  todo,
 }) => {
 
-   const created = (created) => {
-      const month = new Date(created).getMonth() + 1;
-      const date = new Date(created).getDate();
-      const year = new Date(created).getFullYear();
-      return `${month}/${date}/${year}`;
-    };
+  const priorityColor = (priority) => {
+   if (priority == "Low") {
+      return "text-[#febe6b]";
+    } else if (priority == "Medium") {
+      return "text-[#99c7fc]";
+    } else if (priority == "High") {
+      return "text-[#feaaa4]";
+    }
+  }
+
+  const created = (created) => {
+    const month = new Date(created).getMonth() + 1;
+    const date = new Date(created).getDate();
+    const year = new Date(created).getFullYear();
+    return `${month}/${date}/${year}`;
+  };
 
   return (
-    <article className="h-full relative ">
-      <section className="break-words overflow-hidden h-12 w-72">
-        <h1 className="font-figtree text-[0.88rem] xl:text-[1rem] text-[#333]">
-          {title}
-        </h1>
+    <article className="h-full relative">
+      <section className=" h-12 flex">
+        <section className="flex-1 overflow-hidden">
+          <h1 className={`${priorityColor(priority)} font-figtree break-words line-clamp-4 text-[0.88rem] xl:text-[1rem]`}>
+            {title}
+          </h1>
+        </section>
+
+        {/* dropdown for update and delete action */}
+        <Dropdown
+          _id={_id}
+          setUpdatedTask={setUpdatedTask}
+          deleteTodoMutation={deleteTodoMutation}
+          todo={todo}
+        />
       </section>
-      <section className="mt-2  h-fit py-1">
-        <p className="leading-normal tracking-wide line-clamp-4 text-[0.78rem] xs:text-[0.79rem] text-[#333]">
+      <section className=" mt-2 h-[5.5rem] py-1">
+        <p className="leading-normal tracking-wide line-clamp-4 text-[0.75rem] text-[#333]">
           {description}
         </p>
       </section>
-
-      <section className=" w-full flex items-center justify-between absolute bottom-0 p-1">
-        <p className={`${priorityType(priority)} flex items-center text-xs font-semibold py-0.5 px-2 rounded-lg`}>
-            <span className='mr-1'><ChevronsUp size={14}/></span>
-            {priority}
-         </p>
-         <span className=" flex items-center text-xs text-[#333]">
-            <span className='mr-1'><CalendarDays size={12}/></span>
-            {created(createdAt)}
-         </span>
+      <section className=" w-full flex items-center justify-between absolute bottom-0 pb-1">
+        <p
+          className={`${priorityColor(priority)} flex items-center text-xs font-semibold py-0.5 px-2 rounded-lg`}
+        >
+          <span className="mr-1">
+            <ChevronsUp size={14} />
+          </span>
+          {priority}
+        </p>
+        <span className="flex items-center text-xs font-semibold text-[#333]">
+          <span className="mr-1">
+            <Calendar size={14}/>
+          </span>
+          {created(createdAt)}
+        </span>
       </section>
     </article>
   );
@@ -46,10 +75,13 @@ const TaskDetails = ({
 export default TaskDetails;
 
 TaskDetails.propTypes = {
-   _id: PropTypes.string,
-   title: PropTypes.string,
-   description: PropTypes.string,
-   priority: PropTypes.string,
-   createdAt: PropTypes.string,
-   priorityType: PropTypes.func,
-}
+  _id: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  priority: PropTypes.string,
+  createdAt: PropTypes.string,
+  priorityType: PropTypes.func,
+  setUpdatedTask: PropTypes.func,
+  deleteTodoMutation: PropTypes.object,
+  todo: PropTypes.object,
+};
