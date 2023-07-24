f2c85fd519f2781f10a58d90beef5159f7bb295f
Paulo Lopez <geekcrasher@gmail.com>
2023-07-25 01:11:23 +0800

    removed the cancel props and onClick event for reseting the input field

--- client/src/components/Modal/Actions/CreateTaskActions.jsx
+++ client/src/components/Modal/Actions/CreateTaskActions.jsx
@@ -1,9 +1,7 @@
-import PropTypes from "prop-types";
-
-const CreateTaskActions = ({cancel}) => {
+const CreateTaskActions = () => {
   return (
     <section className="mt-6 grid grid-cols-2 gap-2 w-full">
-      <button type="reset" className="btn btn-ghost text-slate-800 normal-case" onClick={cancel}>
+      <button type="reset" className="btn btn-ghost text-slate-800 normal-case">
         Cancel
       </button>
       <button type="submit" className="btn bg-[#2fa199] hover:bg-[#058e85] active:bg-[#2fa199] text-slate-50 normal-case border-0">
@@ -14,7 +12,3 @@ const CreateTaskActions = ({cancel}) => {
 };
 
 export default CreateTaskActions;
-
-CreateTaskActions.propTypes = {
-   cancel: PropTypes.func,
- };
