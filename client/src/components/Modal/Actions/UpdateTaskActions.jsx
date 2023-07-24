5a0dca5ee134ca9ab201ed9ad51c65e1a8c537ac
Paulo Lopez <geekcrasher@gmail.com>
2023-07-24 23:59:38 +0800

    removed the onClick event and change and added type submit

--- client/src/components/Modal/Actions/UpdateTaskActions.jsx
+++ client/src/components/Modal/Actions/UpdateTaskActions.jsx
@@ -1,10 +1,8 @@
-import PropTypes from "prop-types";
-
-const UpdateTaskActions = ({saveUpdatedTask}) => {
+const UpdateTaskActions = () => {
   return (
     <div className="modal-action mt-6 grid grid-cols-2 gap-2 w-full">
       <button className="btn btn-ghost text-slate-800 normal-case">Cancel</button>
-      <button className="btn bg-[#2fa199] hover:bg-[#058e85] active:bg-[#2fa199] text-slate-50 border-0 normal-case" onClick={saveUpdatedTask}>
+      <button type="submit" className="btn bg-[#2fa199] hover:bg-[#058e85] active:bg-[#2fa199] text-slate-50 border-0 normal-case" >
         Save Changes
       </button>
     </div>
@@ -12,7 +10,3 @@ const UpdateTaskActions = ({saveUpdatedTask}) => {
 };
 
 export default UpdateTaskActions;
-
-UpdateTaskActions.propTypes = {
-   saveUpdatedTask: PropTypes.func
-}
