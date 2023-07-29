553efec992fd78e72dbcc807093378468c282630
Paulo Lopez <geekcrasher@gmail.com>
2023-07-29 23:36:27 +0800

    removed the import react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,10 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from './components/Header/Header'
 import TodoList from "./components/TodoList/TodoList";
-import { Route, Router, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <main className="min-h-screen w-full relative flex flex-col bg-white">
+    <main className="min-h-screen w-full relative flex flex-col bg-white dark:bg-main_dark">
       <Header />
       <TodoList />
       <Footer />
