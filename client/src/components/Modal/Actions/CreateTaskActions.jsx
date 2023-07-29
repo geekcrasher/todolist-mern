ab484af3da4c8a60a047cc483eb00efa8b5f0729
Paulo Lopez <geekcrasher@gmail.com>
2023-07-29 23:48:20 +0800

    removed the onClick event to the

--- client/src/components/Modal/Actions/CreateTaskActions.jsx
+++ client/src/components/Modal/Actions/CreateTaskActions.jsx
@@ -1,10 +1,10 @@
 const CreateTaskActions = () => {
   return (
-    <section className="mt-6 grid grid-cols-2 gap-2 w-full">
+    <section className="mt-8 grid grid-cols-2 gap-2 w-full">
       <button type="reset" className="btn btn-ghost text-slate-800 normal-case">
         Cancel
       </button>
-      <button type="submit" className="btn bg-[#2fa199] hover:bg-[#058e85] active:bg-[#2fa199] text-slate-50 normal-case border-0">
+      <button  type="submit" className="btn  bg-[#2fa199] hover:bg-[#058e85] active:bg-[#2fa199] text-slate-50 normal-case border-0">
         Create
       </button>
     </section>
--- client/src/components/Modal/Actions/UpdateTaskActions.jsx
+++ client/src/components/Modal/Actions/UpdateTaskActions.jsx
@@ -1,11 +1,11 @@
 const UpdateTaskActions = () => {
   return (
-    <div className="modal-action mt-6 grid grid-cols-2 gap-2 w-full">
+    <section className="modal-action mt-6 grid grid-cols-2 gap-2 w-full">
       <button className="btn btn-ghost text-slate-800 normal-case">Cancel</button>
       <button type="submit" className="btn bg-[#2fa199] hover:bg-[#058e85] active:bg-[#2fa199] text-slate-50 border-0 normal-case" >
         Save Changes
       </button>
-    </div>
+    </section>
   );
 };
 
